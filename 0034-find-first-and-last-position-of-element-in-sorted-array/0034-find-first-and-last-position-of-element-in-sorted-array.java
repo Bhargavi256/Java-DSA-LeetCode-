6212953ed76acbaae91b547ee5b0fbebee98bde0
Sudhaveni Bhargavi <60396502+Bhargavi256@users.noreply.github.com>
2023-07-04 15:14:27 +0530

    Time: 11 ms (16.85%), Space: 44.6 MB (50.96%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,27 +1,46 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] a = new int[2];
-        int f=0,g=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){
-              a[0]=i;
-              f=1;
-              break;
-              }
+        int a[] = new int[2];
+        int res = -1;
+        int start = 0;
+        int end = nums.length-1;
+        int mid=0;
+         System.out.print(start+" start ");
+         System.out.print(mid+" mid ");
+         System.out.println(end+" end ");
+        while(start<=end){
+             mid =start + (end-start)/2;
+            if(nums[mid]==target){
+                System.out.print(mid+"mid");
+              res   = mid;
+                end = mid-1;
+            }
+            else if(target<nums[mid])
+            {
+                end = mid-1;
+                 System.out.print(end+"end");
+            }
+            else 
+                start = mid+1;
+             System.out.print(start+"start");
         }
-        for(int j=nums.length-1;j>0;j--){
-            if(nums[j]==target){
-              a[1]=j;
-              g=1;
-              break;
-              }
+       a[0]=res;
+         start = 0;
+         end = nums.length-1;
+        while(start<=end){
+             mid = (start+end)/2;
+            if(nums[mid]==target){
+                res = mid;
+                start = mid+1;
+            }
+            else if(target<nums[mid])
+            {
+                end = mid-1;
+            }
+            else 
+                start = mid+1;
         }
-        if(f!=1&&g!=1)
-           {
-               a[0]=-1;
-               a[1]=-1;
-           }
-    return a;
-
+      a[1] = res;
+        return a;
     }
 }
