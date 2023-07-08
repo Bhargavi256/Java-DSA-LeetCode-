3be470baf783d20aae6a8bbb8fe40dfb05a42e64
Sudhaveni Bhargavi <60396502+Bhargavi256@users.noreply.github.com>
2023-07-08 16:00:16 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (87.24%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,17 +1,45 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int peak = 0;
-        if(nums.length==1)
-            return 0;
-        for(int i=1;i<nums.length-1;i++){
-            if(nums[i]>nums[i-1] && nums[i]>nums[i+1])
-                peak =i;
+//         int peak = 0;
+//         if(nums.length == 1) return 0;
+//        int start = 0, end = nums.length-1;
+        
+//         int n = nums.length;
+//         if(nums[0] > nums[1]) return 0;
+//         if(nums[n-1] > nums[n-2]) return n-1;
+//         while(start<=end){
+//             int mid = start + (end -start)/2;
+//             if(mid>0 && mid <n - 1){
+//                 if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+//                     return mid;
+//                 }
+//                 else if(nums[mid-1]>nums[mid])
+//                     end = start-1;
+//                 else
+//                     start = mid+1;
+//             }
+            
+//         }
+//         return 0;
+        
+        if(nums.length == 1) return 0; // single element
+        
+        int n = nums.length;
+        
+		// check if 0th/n-1th index is the peak element
+        if(nums[0] > nums[1]) return 0;
+        if(nums[n-1] > nums[n-2]) return n-1;
+		
+		// search in the remaining array
+        int start = 1;
+        int end = n-2;
+        
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) return mid;
+            else if(nums[mid] < nums[mid-1]) end = mid - 1;
+            else if(nums[mid] < nums[mid+1]) start = mid + 1;
         }
-        if(nums[0]>nums[1])
-            return 0;
-        if(nums[nums.length-1]>nums[nums.length-2]){
-            return nums.length-1;
-        }
-        return peak;
+        return -1; // dummy return statement
     }
 }
