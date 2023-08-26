9142e4e70c038f47db82067383798afe98e9a3a7
Sudhaveni Bhargavi <60396502+Bhargavi256@users.noreply.github.com>
2023-08-26 07:17:17 +0530

    Time: 1 ms (94.21%), Space: 44 MB (52.00%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,29 @@
+class Solution {
+    
+    public void recurPer(int ind,int[] nums,List<List<Integer>> ans){
+        if(ind==nums.length){
+            List<Integer> ds = new ArrayList<>();
+            for(int i=0;i<nums.length;i++){
+                ds.add(nums[i]);
+            }
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+    for(int i=ind;i<nums.length;i++){
+        swap(i,ind,nums);
+        recurPer(ind+1,nums,ans);
+        swap(i,ind,nums);
+    }
+    }
+    public void swap(int i,int j,int[] nums){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+    public List<List<Integer>> permute(int[] nums) {
+       List<List<Integer>> ans = new ArrayList<>();
+        recurPer(0,nums,ans);
+        return ans;
+    }
+}
